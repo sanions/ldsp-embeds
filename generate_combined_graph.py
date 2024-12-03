5a957667cbf8ceeac0b82969af436180681bcbe2
Saniya Karwa <saniya@mit.edu>
2024-12-03 15:25:13 -0500

    added PCA to combined graphs

--- generate_combined_graph.py
+++ generate_combined_graph.py
@@ -6,13 +6,15 @@ from tqdm import tqdm
 from utils import *
 
 
-def create_combined_graph(mutual_info_df, ttest_results_df, clf_weights_df, results_directory, N=20):
+def create_combined_graph(mutual_info_df, ttest_results_df, clf_weights_df, pca_contribs_df, results_directory, N=20):
    
     mutual_informations = mutual_info_df['Mutual_Information'].values
 
     top_N_ttest_dimensions = ttest_results_df.nsmallest(N, 'p_value')['dimension'].values
 
     top_N_clf_dimensions = clf_weights_df.nlargest(N, 'Weight')['Dimension'].values
+
+    top_N_pca_contribs = pca_contribs_df.nlargest(N, 'Contribution')['Dimension'].values
 
     plt.figure(figsize=(12, 6))
 
@@ -21,8 +23,9 @@ def create_combined_graph(mutual_info_df, ttest_results_df, clf_weights_df, resu
     threshold = np.sort(mutual_informations)[-N]
     plt.axhline(y=threshold, color='r', linestyle='--', label=f'Top {N} Threshold')
 
-    plt.scatter(top_N_ttest_dimensions, mutual_informations[top_N_ttest_dimensions], color='red', label='Top N (t-test)', s=50, alpha=0.4)
-    plt.scatter(top_N_clf_dimensions, mutual_informations[top_N_clf_dimensions], color='blue', label='Top N (Logistic Reg.)', s=50, alpha=0.4)
+    plt.scatter(top_N_ttest_dimensions, mutual_informations[top_N_ttest_dimensions], color='red', marker='s', label=f'Top {N} (t-test)', s=50, alpha=0.4)
+    plt.scatter(top_N_clf_dimensions, mutual_informations[top_N_clf_dimensions], color='blue', label=f'Top {N} (Logistic Reg.)', s=50, alpha=0.4)
+    plt.scatter(top_N_pca_contribs, mutual_informations[top_N_pca_contribs], color='green', marker='^', label=f'Top {N} (PCA)', s=50, alpha=0.8)
 
     plt.xlabel("Embedding Dimension")
     plt.ylabel("Mutual Information")
@@ -42,11 +45,12 @@ if __name__ == "__main__":
     embedding_filepaths = get_embeddings_filepaths()
 
     for embeddings_csv in tqdm(embedding_filepaths):
-
         results_directory = get_results_directory(embeddings_csv, "combined_analysis")
 
         mutual_info_df = pd.read_csv(os.path.join(get_results_directory(embeddings_csv, "mutual_information"), "mutual_information_all.csv"))
         ttest_results_df = pd.read_csv(os.path.join(get_results_directory(embeddings_csv, "t_test_analysis"), "t_test_results.csv"))
         clf_weights_df = pd.read_csv(os.path.join(get_results_directory(embeddings_csv, "logistic_classifier"), "classifier_weights.csv"))
+        pca_contribs_df = pd.read_csv(os.path.join(get_results_directory(embeddings_csv, "pca_analysis"), "pca_all.csv"))
 
-        create_combined_graph(mutual_info_df, ttest_results_df, clf_weights_df, results_directory, N=20)
+        create_combined_graph(mutual_info_df, ttest_results_df, clf_weights_df, pca_contribs_df, results_directory, N=20)
+
